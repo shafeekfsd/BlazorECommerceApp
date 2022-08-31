e0e7b2023fc9bfec138dae5d5369d1d45f241a11
Muhammad Shafeek <shafeekfsd@gmail.com>
2022-09-01 01:51:21 +0530

    Add project files.

---
+++ BlazorECommerceApp/Server/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorECommerceApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly BlazorECommerceDbContext _blazorECommerceDbContext;
+
+        public ProductController(BlazorECommerceDbContext blazorECommerceDbContext)
+        {
+            _blazorECommerceDbContext = blazorECommerceDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Index()
+        {
+           var products = await _blazorECommerceDbContext.Products.ToListAsync();   
+            return Ok(products);
+        }
+
+
+
+        //    private static List<Product> Products = new List<Product>
+        //{
+        //    new Product
+        //    {
+        //        Id=1,
+        //        Title="The Hitchhiker's Guide to the Galaxy",
+        //        Description=" The Hitchhiker's Guide to the Galaxy[note 1] (sometimes referred to as HG2G,[1] HHGTTG,[2] H2G2,[3] or tHGttG) is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including novels, stage shows, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film.",
+        //        ImageUrl="https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+        //        Price=9.99m
+        //    },
+        //    new Product
+        //    {
+        //        Id=2,
+        //        Title="Ready Player One",
+        //        Description="Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House).[1] The book was published on August 16, 2011.[2] An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.[3][4]Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association[5] and won the 2011 Prometheus Award.[6]",
+        //        ImageUrl="https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
+        //        Price=10.99m
+        //    },
+        //    new Product
+        //    {
+        //        Id=3,
+        //        Title="Snow Crash",
+        //        Description="Snow Crash is a science fiction novel by the American writer Neal Stephenson, published in 1992. Like many of Stephenson's novels, it covers history, linguistics, anthropology, archaeology, religion, computer science, politics, cryptography, memetics, and philosophy",
+        //        ImageUrl="https://upload.wikimedia.org/wikipedia/en/d/d5/Snowcrash.jpg",
+        //        Price=8.09m
+        //    },
+        //    new Product
+        //    {
+        //        Id=4,
+        //        Title="Dune (2021 film)",
+        //        Description="Dune (titled onscreen as Dune: Part One) is a 2021 American epic science fiction film directed by Denis Villeneuve from a screenplay by Villeneuve, Jon Spaihts, and Eric Roth. It is the first of a two-part adaptation of the 1965 novel by Frank Herbert, primarily covering the first half of the book. Set in the distant future, the film follows Paul Atreides as his family, the noble House Atreides, is thrust into a war for the deadly and inhospitable desert planet Arrakis. The ensemble cast includes Timothée Chalamet, Rebecca Ferguson, Oscar Isaac, Josh Brolin, Stellan Skarsgård, Dave Bautista, Stephen McKinley Henderson, Zendaya, David Dastmalchian, Chang Chen, Sharon Duncan-Brewster, Charlotte Rampling, Jason Momoa, and Javier Bardem.",
+        //        ImageUrl="https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
+        //        Price=6.09m
+        //    }
+        //};
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetProduct()
+        //{
+        //    return Ok(Products);
+        //}
+    }
+
+}
